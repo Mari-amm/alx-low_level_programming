e49e907645ba97c0d07a2b5b6a772a9cdb2a7700
maryam <nessafmaryam@gmail.com>
2023-07-13 15:27:04 -0700

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Prints "with proper grammar but the outcome is
  *              a piece of art," followed by a new line.
- *               ok
+ *               
  *
  * Return: 0 
  */
