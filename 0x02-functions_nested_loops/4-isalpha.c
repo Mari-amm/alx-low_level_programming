b6cbbf747d3005b35fa136ee9337bcab1237c5ca
maryam <nessafmaryam@gmail.com>
2023-07-19 18:28:23 -0700

    save this

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,30 +1,15 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * _isalpha - Checks if a character is a letter.
+ * @c: The character to  check.
+ *
+ * Return: 1 if 'c' is a letter,
+ * return  0 otherwise.
  */
-void times_table(void)
+int _isalpha(int c)
 {
-	int num, mult, prod;
-
-	for (v = 0; v <= 9; v++)
-	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = v * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
 }
