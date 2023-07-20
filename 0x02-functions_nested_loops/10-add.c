37b4ec469327b491eeb9dcd15eaba1e9d283e066
maryam <nessafmaryam@gmail.com>
2023-07-19 19:04:52 -0700

    save file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,5 +12,3 @@ int add(int num1, int num2)
 {
         return (num1 + num2);
 }
-~                                                                                                                                                                        
-~                                
