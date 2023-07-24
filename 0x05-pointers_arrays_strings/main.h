004a247ecb00bd797024c79b4fc6913f3065d1ee
maryam <nessafmaryam@gmail.com>
2023-07-24 09:18:22 -0700

    ok

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd_h>
 
 int _putchar(char c)
 void reset_to_98(int *n);
@@ -18,5 +19,5 @@ int _atoi(char *s);
 
 
 
-#endif /*MAIN_H */
+#endif
 
