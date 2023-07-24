222a72da3fde217b40db3cade0fb953d9766b546
maryam <nessafmaryam@gmail.com>
2023-07-24 08:30:37 -0700

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * rest_to_98  pointer to an int as parameter and updates the value it points to to 98
+ *
+ *
+ * main_main pointers
+ * 
+ * Return: Always 0.
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
