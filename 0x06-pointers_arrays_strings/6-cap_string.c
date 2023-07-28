8b9902c3d74834414e77bf449b4a161aa566ec8c
maryam <nessafmaryam@gmail.com>
2023-07-27 18:03:31 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,30 @@
 #include "main.h"
-
 /**
- * string_toupper - a function that changes all
- * lowercase letters of a string to uppercase.
- * @t : input char
- * Return: @t
-*/
-char *string_toupper(char *t)
-{
-	int p;
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
-	for (p = 0; t[p] != '\0'; p++)
+char *cap_string(char *s)
+{
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (t[p] > 96 && t[p] < 123)
+		for (i = 0; i < 13; i++)
 		{
-			t[p] -= 32;
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
+		count++;
 	}
-	return (t);
+	return (s);
 }
