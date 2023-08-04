dca38644f54266aff24bbd8a3901e85a1bbf819b
maryam <nessafmaryam@gmail.com>
2023-08-04 16:58:10 -0700

    ok

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
-//**
- * _isdigit - checks 0 - 9.
+/**
+ * _isdigit - checks 0 - 9
  *
  * @c: input
  *
- * Return: 1 if digit, 0 if not.
+ * Return: 1 if digit, 0 if not
 */
 
 int _isdigit(int c)
