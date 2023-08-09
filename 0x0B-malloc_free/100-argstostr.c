665544d4f4896752ea6f9491b3b83aa94d46e30e
maryam <nessafmaryam@gmail.com>
2023-08-08 18:35:34 -0700

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,48 +9,48 @@
 
 char *argstostr(int ac, char **av)
 {
-char *new_string = NULL;
-int B = 0, i = ac, r, summe = 0, tm = 0;
+		char *new_string = NULL;
+		int B = 0, i = ac, j, summeme = 0, tm = 0;
 
-if (ac == 0 || av == NULL)
-return (NULL);
+		if (ac == 0 || av == NULL)
+			return (NULL);
 
-while (ac--)
-summe += (len(av[ac]) + 1);
-new_string = (char *) malloc(summe + 1);
+		while (ac--)
+			summeme += (lenght(av[ac]) + 1);
+		new_string = (char *) malloc(summeme + 1);
 
-if (new_string != NULL)
-{
-while (B < i)
-{
-for (r = 0; av[B][r] != '\0'; r++)
-new_string[r + tm] = av[B][r];
-new_string[tm + r] = '\n';
-tm += (r + 1);
-B++;
-}
-new_string[tm] = '\0';
-}
-else
-{
-return (NULL);
-}
-return (new_string);
+		if (new_string != NULL)
+		{
+			while (B < i)
+			{
+				for (j = 0; av[B][j] != '\0'; j++)
+					new_string[j + tm] = av[B][j];
+				new_string[tm + j] = '\n';
+				tm += (j + 1);
+				B++;
+			}
+			new_string[tm] = '\0';
+		}
+		else
+		{
+			return (NULL);
+		}
+		return (new_string);
 }
 
 /**
-*len - returns length of str
+*lenght - returns length of str
 *@str: string counted
 *Return: returns the length
 */
-int len(char *str)
+int lenght(char *str)
 {
-int len = 0;
+		int lenght = 0;
 
-if (str != NULL)
-{
-while (str[len])
-len++;
-}
-return (len);
+		if (str != NULL)
+		{
+			while (str[lenght])
+				lenght++;
+		}
+	return (lenght);
 }
